5e2183fd25b2337b4897238087ceef5fd7d57deb
Emi Boucly <bouclyma@etu.univ-grenoble-alpes.fr>
2025-06-03 11:02:10 +0200

    feat(88x31): ajout des liens

--- frontend/src/components/88x31.vue
+++ frontend/src/components/88x31.vue
@@ -6,38 +6,38 @@
   <h3 class="center">There, have cool old-fashioned 88x31 buttons</h3>
   <div class="wrapper flex">
     <a href="https://tralala.fr.eu.org" title="tralala.fr.eu.org"><img src="../components/icons/tralala-88x31.gif" alt="tralala.fr.eu.org" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/estrogen-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/18carat-88x31.png" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/aceattorney-88x31.png" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/adobe-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/archlinux-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/curl-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/discord-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/evangelion-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/firefox-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/gitea-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/gnu-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/hl-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/java-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/jjba-88x31.png" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/linux-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/mars-88x31.png" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/miku1-88x31.png" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/miku2-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/ms_88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/steam-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/suntooth-88x31.png" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/techromancer-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/teto1-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/teto2-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/tokyotelecom-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/tor-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/touhou-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/touhou2-88x31.png" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/vim-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/virtuaverse-88x31.gif" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/vocaloid1-88x31.png" alt="" height="31" width="88"></a>
-    <a href=""><img src="../components/icons/winrar-88x31.gif" alt="" height="31" width="88"></a>
+    <a href="https://fransgenre.fr/" title="fransgenre.fr"><img src="../components/icons/estrogen-88x31.gif" alt="fransgenre.fr" height="31" width="88"></a>
+    <a href="https://www.youtube.com/watch?v=vz-ScKO1BnM" title="18carat"><img src="../components/icons/18carat-88x31.png" alt="18carat" height="31" width="88"></a>
+    <a href="https://www.ace-attorney.com/" title="ace-attorney.org"><img src="../components/icons/aceattorney-88x31.png" alt="ace-attorney.org" height="31" width="88"></a>
+    <a href="https://flashpointarchive.org/" title="flashpointarchive.org"><img src="../components/icons/adobe-88x31.gif" alt="flashpointarchive.org" height="31" width="88"></a>
+    <a href="https://archlinux.org/" title="archlinux.org"><img src="../components/icons/archlinux-88x31.gif" alt="archlinux.org" height="31" width="88"></a>
+    <a href="https://curl.se/" title="curl.se"><img src="../components/icons/curl-88x31.gif" alt="curl.se" height="31" width="88"></a>
+    <a href="https://discord.com/" title="discord.com"><img src="../components/icons/discord-88x31.gif" alt="discord.com" height="31" width="88"></a>
+    <a href="https://www.evangelion.jp/" title="evangelion.jp"><img src="../components/icons/evangelion-88x31.gif" alt="evangelion.jp" height="31" width="88"></a>
+    <a href="https://www.mozilla.org/fr/firefox" title="firefox"><img src="../components/icons/firefox-88x31.gif" alt="firefox" height="31" width="88"></a>
+    <a href="https://about.gitea.com/" title="about.gitea.com"><img src="../components/icons/gitea-88x31.gif" alt="about.gitea.com" height="31" width="88"></a>
+    <a href="https://www.gnu.org/" title="gnu.org"><img src="../components/icons/gnu-88x31.gif" alt="gnu.org" height="31" width="88"></a>
+    <a href="https://www.half-life.com/" title="half-life.com"><img src="../components/icons/hl-88x31.gif" alt="half-life.com" height="31" width="88"></a>
+    <a href="https://www.java.com/" title="java.com"><img src="../components/icons/java-88x31.gif" alt="java.com" height="31" width="88"></a>
+    <a href="https://jojo-portal.com/en/" title="jojo-portal.com"><img src="../components/icons/jjba-88x31.png" alt="jojo-portal.com" height="31" width="88"></a>
+    <a href="https://www.linux.org/" title="linux.org"><img src="../components/icons/linux-88x31.gif" alt="linux.org" height="31" width="88"></a>
+    <a href="https://subsetgames.com/ftl.html" title="subsetgames.com"><img src="../components/icons/mars-88x31.png" alt="subsetgames.com" height="31" width="88"></a>
+    <a href="https://youtu.be/LaEgpNBt-bQ?si=njsOoX2RmBiteFAf&t=85" title="mikumikubeam"><img src="../components/icons/miku1-88x31.png" alt="mikumikubeam" height="31" width="88"></a>
+    <a href="https://www.youtube.com/watch?v=19y8YTbvri8" title="mesmerizzer"><img src="../components/icons/miku2-88x31.gif" alt="mesmerizzer" height="31" width="88"></a>
+    <a href="https://www.wikihow.com/Remove-Microsoft-Edge" title="removeedge"><img src="../components/icons/ms_88x31.gif" alt="removeedge" height="31" width="88"></a>
+    <a href="https://store.steampowered.com/" title="vapeur"><img src="../components/icons/steam-88x31.gif" alt="vapeur" height="31" width="88"></a>
+    <a href="https://www.doctorwho.tv/" title="docteurqui"><img src="../components/icons/suntooth-88x31.png" alt="docteurqui" height="31" width="88"></a>
+    <a href="https://www.torproject.org/fr/download/"><img src="../components/icons/techromancer-88x31.gif" alt="" height="31" width="88"></a>
+    <a href="https://www.youtube.com/watch?v=duPJqfKiA78" title="lestroisbakas"><img src="../components/icons/teto1-88x31.gif" alt="lestroisbakas" height="31" width="88"></a>
+    <a href="https://www.youtube.com/watch?v=JALbemLw3G4" title="tetoterritoire"><img src="../components/icons/teto2-88x31.gif" alt="tetoterritoire" height="31" width="88"></a>
+    <a href="http://websdr.ewi.utwente.nl:8901/" title="websdr"><img src="../components/icons/tokyotelecom-88x31.gif" alt="websdr" height="31" width="88"></a>
+    <a href="https://www.torproject.org/download/" title="oignon"><img src="../components/icons/tor-88x31.gif" alt="oignon" height="31" width="88"></a>
+    <a href="https://www.youtube.com/watch?v=P4dnkdikJMw" title="tohno"><img src="../components/icons/touhou-88x31.gif" alt="tohno" height="31" width="88"></a>
+    <a href="https://github.com/trung-kieen/bad-apple-ascii" title="mauvaisepomme"><img src="../components/icons/touhou2-88x31.png" alt="mauvaisepomme" height="31" width="88"></a>
+    <a href="https://www.vim.org/" title="vim"><img src="../components/icons/vim-88x31.gif" alt="vim" height="31" width="88"></a>
+    <a href="https://www.youtube.com/watch?v=Ht9tf0nRb6s" title="virtuaverse"><img src="../components/icons/virtuaverse-88x31.gif" alt="virtuaverse" height="31" width="88"></a>
+    <a href="https://www.youtube.com/watch?v=0WMr1dRUYe4" title="kafu"><img src="../components/icons/vocaloid1-88x31.png" alt="kafu" height="31" width="88"></a>
+    <a href="https://www.win-rar.com/start.html?&L=0" title="vinrare"><img src="../components/icons/winrar-88x31.gif" alt="vinrare" height="31" width="88"></a>
 
 
   </div>
